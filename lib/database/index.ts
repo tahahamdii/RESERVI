7872063ab2f296ba415a9134985811ef2e0a2985
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-01-31 20:15:07 +0100

    db

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -5,13 +5,18 @@ const MONGODB_URI = process.env.MONGODB_URI;
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
+
   if (cached.conn) return cached.conn;
 
   if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'reservi',
+
+
+    // dbName: 'reservi',
     bufferCommands: false,
+
+
   })
 
   cached.conn = await cached.promise;
