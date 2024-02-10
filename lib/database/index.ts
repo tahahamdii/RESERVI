2baa288cd6c02c523ba88f52d147f4ed54eecaae
Hamdi Taha <hamdi.taha@esprit.tn>
2024-02-10 20:29:18 +0100

    connecting2

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -5,15 +5,23 @@ const MONGODB_URI = process.env.MONGODB_URI;
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
+  console.log('connectToDatabase function called');
 
-  if (cached.conn) return cached.conn;
+  if (cached.conn) {
+    console.log('Using existing database connection');
+    return cached.conn;
+  }
 
   if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
     dbName: 'reservii',
     bufferCommands: false,
-  })
+  }).then(() => {
+    console.log('Connected to MongoDB...');
+  }).catch((error) => {
+    console.error('Could not connect to MongoDB...', error);
+  });
 
   cached.conn = await cached.promise;
 
